c1a3c326a3d3854b467f0b82d8c1b8a638451ef5
Mishal Alfawaz <77988622+MishalFayez@users.noreply.github.com>
2023-05-30 14:46:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 
 from langchain.llms import OpenAI
@@ -52,10 +52,37 @@ from langchain import ConversationChain
 conversation = ConversationChain(llm=llm, verbose=True)
 
 
-conversation.predict(input="Hi there")
+# conversation.predict(input="Hi there")
 
-conversation.predict(input="I'm doing well just having a conversation with an AI")
+# conversation.predict(input="I'm doing well just having a conversation with an AI")
 
-conversation.predict(input="What was the first thing i said to you?")
+# conversation.predict(input="What was the first thing i said to you?")
 
-conversation.predict(input="what is an alternative phrase for the first thing i said to you?")
+# conversation.predict(input="what is an alternative phrase for the first thing i said to you?")
+
+
+#-- memory --
+
+from langchain.memory import ChatMessageHistory
+from langchain.chat_models import ChatOpenAI
+openai_api_key = ""
+chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+
+history = ChatMessageHistory()
+
+history.add_ai_message("Hi!")
+history.add_user_message("what is the capital of france?")
+print(history.messages[0])
+ai_response = chat(history.messages)
+
+print(ai_response.content)
+history.add_ai_message(ai_response.content)
+
+print("History: ", history.messages)
+
+history.add_user_message("what was my first thing i said to you?")
+
+ai_response2 = chat(history.messages)
+
+history.add_ai_message(ai_response2.content)
+print(history.messages)
